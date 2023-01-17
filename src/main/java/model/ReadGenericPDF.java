c679d28b6e9a5e7beb93b6f7a209dfc7e8b2148f
Luigi D'Antonio <93997797+LuigiSiddhart@users.noreply.github.com>
2023-01-17 12:54:27 +0100

    Update ReadGenericPDF.java

--- src/main/java/model/ReadGenericPDF.java
+++ src/main/java/model/ReadGenericPDF.java
@@ -9,10 +9,6 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.text.PDFTextStripper;
 
-/**
- *
- * @author danto
- */
 public class ReadGenericPDF {
 
     File file;
@@ -21,12 +17,6 @@ public class ReadGenericPDF {
         this.file = file;
     }
 
-    /**
-     *
-     * @param file
-     * @return Elaborated Text
-     * @throws java.io.IOException
-     */
     public String extractText(File file) throws IOException {
         String totaltext = null;
         try (PDDocument document = PDDocument.load(file)) {
@@ -41,14 +31,11 @@ public class ReadGenericPDF {
                 // Set the page interval to extract. If you don't, then all pages would be extracted.
                 stripper.setStartPage(p);
                 stripper.setEndPage(p);
-
-                // let the magic happen
                 String text = stripper.getText(document);
                 totaltext += text;
                 String textLenghtString = String.format("Text lenght of page %d:", p);
                 System.out.println(textLenghtString);
                 System.out.println(text.length() + "\n");
-                // do some nice output with a header
                 String pageStr = String.format("page %d:", p);
                 System.out.println(pageStr);
                 for (int i = 0; i < pageStr.length(); ++i) {
@@ -65,12 +52,10 @@ public class ReadGenericPDF {
     }
 
     public void extractGeneralInformation(String text) {
-        // Read Pratictionar Name and Patient Identifier
-
         Pattern pattern = Pattern.compile("Medico :\\s*(\\w+)\\s+(\\w+)");
         Matcher matcher = pattern.matcher(text);
         if (matcher.find()) {
-            String nomeMedico = matcher.group(1);        //309341008
+            String nomeMedico = matcher.group(1);     
             String cognomeMedico = matcher.group(2);
             System.out.println("Nome e cognome del medico: " + nomeMedico + " " + cognomeMedico);
         }

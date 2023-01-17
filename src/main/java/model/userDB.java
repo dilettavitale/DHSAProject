733b243bc5373f43e1d28d4f6f21009e878e0ffe
Luigi D'Antonio <93997797+LuigiSiddhart@users.noreply.github.com>
2023-01-17 13:02:11 +0100

    Update userDB.java

--- src/main/java/model/userDB.java
+++ src/main/java/model/userDB.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author immacolata
- */
 public interface userDB {
     
     public abstract UserModel findUser(String cf, String password) throws Exception;

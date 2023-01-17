0150b406ed1ca6ba6d7976fe1fb743263bb2d65d
Luigi D'Antonio <93997797+LuigiSiddhart@users.noreply.github.com>
2023-01-17 13:03:30 +0100

    Update GeneralFactory.java

--- src/main/java/modelfactory/GeneralFactory.java
+++ src/main/java/modelfactory/GeneralFactory.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package modelfactory;
 
 import model.GeneralModel;
 import model.UserModel;
 
-/**
- *
- * @author immacolata
- */
 public class GeneralFactory extends UserFactory {
     @Override
     protected GeneralModel selectRole(Role role) {
@@ -21,6 +14,4 @@ public class GeneralFactory extends UserFactory {
                 return null;
         }
     }
-
-
 }
